874a4e8dd24b7ba0633c865b4012cfbfc21bb440
Glevinzon Dapal <glevinzon@sci.ph>
2017-02-23 13:26:51 +0800

    applied semantic ui

--- src/components/Equation/Equation.js
+++ src/components/Equation/Equation.js
@@ -1,11 +1,40 @@
 import React, { Component, PropTypes } from 'react'
-import {Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
+import EquationList from './EquationList'
+import EquationForm from './EquationForm'
+import { Input, Menu, Segment } from 'semantic-ui-react'
+
+const MenuItem = Menu.Item
+const MenuMenu = Menu.Menu
 
 class componentName extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      activeItem: 'browse'
+    }
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render () {
-    console.log(this.props)
+    const { activeItem } = this.state
     return (
-      <div />
+      <div>
+        <Menu pointing>
+          <MenuItem name='browse' active={activeItem === 'browse'} onClick={this.handleItemClick} />
+          <MenuItem name='submit' active={activeItem === 'submit'} onClick={this.handleItemClick} />
+          <MenuMenu position='right'>
+            <MenuItem>
+              <Input icon='search' placeholder='Search...' />
+            </MenuItem>
+          </MenuMenu>
+        </Menu>
+        <Segment>
+          {activeItem === 'browse' ? <EquationList {...this.props} /> : null}
+          {activeItem === 'submit' ? <EquationForm {...this.props} /> : null}
+        </Segment>
+
+      </div>
     )
   }
 }
