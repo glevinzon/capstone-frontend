7b17702af4d294ddf9513beacc843af583c33c3f
Glevinzon Dapal <glevinzon@sci.ph>
2017-02-23 13:57:50 +0800

    added multiple select

--- src/components/Equation/EquationForm.js
+++ src/components/Equation/EquationForm.js
@@ -1,11 +1,33 @@
 import React, { Component, PropTypes } from 'react'
-import { Form, Input } from 'semantic-ui-react'
+import { Form, Input, Dropdown } from 'semantic-ui-react'
 
 const FormGroup = Form.Group
 const FormField = Form.Field
 
 class EquationForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      stateOptions: [
+        { key: 'English', text: 'English', value: 'English' },
+        { key: 'French', text: 'French', value: 'French' },
+        { key: 'Spanish', text: 'Spanish', value: 'Spanish' },
+        { key: 'German', text: 'German', value: 'German' },
+        { key: 'Chinese', text: 'Chinese', value: 'Chinese' }
+      ]
+    }
+  }
+
+  handleAddition = (e, { value }) => {
+    this.setState({
+      stateOptions: [{ text: value, value }, ...this.state.stateOptions]
+    })
+  }
+
+  handleChange = (e, { value }) => this.setState({ stateOptions: value })
+
   render () {
+    const { stateOptions } = this.state
     return (
       <Form>
         <FormGroup widths='equal'>
@@ -19,7 +41,18 @@ class EquationForm extends Component {
           </FormField>
           <FormField>
             <label>Tags</label>
-            <Input placeholder='Tags' />
+            <Dropdown
+              options={this.state.stateOptions}
+              placeholder='Choose Languages'
+              search
+              selection
+              fluid
+              multiple
+              allowAdditions
+              value={stateOptions}
+              onAddItem={this.handleAddition}
+              onChange={this.handleChange}
+            />
           </FormField>
         </FormGroup>
       </Form>
