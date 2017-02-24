931b877e154824863e69762504ee9d1540264367
Glevinzon Dapal <glevinzon.zeal@gmail.com>
2017-02-25 00:15:20 +0800

    added loading dimmer after submitting from

--- src/components/Equation/EquationForm.js
+++ src/components/Equation/EquationForm.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import { Button, Form, Input, Dropdown } from 'semantic-ui-react'
+import { Button, Form, Input, Dropdown, Dimmer, Loader} from 'semantic-ui-react'
 import validateInput from 'utils/validators/equation'
 
 const FormGroup = Form.Group
@@ -27,8 +27,12 @@ class EquationForm extends Component {
   }
 
   componentWillReceiveProps (nextProps) {
+    console.log(nextProps)
     if (nextProps.equations.creatingShop) {
       this.setState({ isLoading: true })
+    }
+    if (nextProps.equations.createSuccess) {
+      this.setState({ name: '', note: '', errors: [], isLoading: false, currentValues: [] })
     }
   }
 
@@ -66,36 +70,41 @@ class EquationForm extends Component {
   }
 
   render () {
-    const { currentValues } = this.state
+    const { currentValues, isLoading } = this.state
     return (
-      <Form onSubmit={this.onSubmit}>
-        <FormGroup widths='equal'>
-          <FormField>
-            <label>Equation</label>
-            <Input placeholder='Name' name='name' value={this.state.name} onChange={this.onTextChange} />
-          </FormField>
-          <FormField>
-            <label>Note</label>
-            <Input placeholder='Description' name='note' value={this.state.note} onChange={this.onTextChange} />
-          </FormField>
-          <FormField>
-            <label>Tags</label>
-            <Dropdown
-              options={this.state.options}
-              placeholder='Choose Tags'
-              search
-              selection
-              fluid
-              multiple
-              allowAdditions
-              value={currentValues}
-              onAddItem={this.handleAddition}
-              onChange={this.handleChange}
-            />
-          </FormField>
-          <Button type='submit'>Submit</Button>
-        </FormGroup>
-      </Form>
+      <div>
+        <Dimmer active={isLoading}>
+          <Loader size='large'>Loading</Loader>
+        </Dimmer>
+        <Form onSubmit={this.onSubmit}>
+          <FormGroup widths='equal'>
+            <FormField>
+              <label>Equation</label>
+              <Input placeholder='Name' name='name' value={this.state.name} onChange={this.onTextChange} />
+            </FormField>
+            <FormField>
+              <label>Note</label>
+              <Input placeholder='Description' name='note' value={this.state.note} onChange={this.onTextChange} />
+            </FormField>
+            <FormField>
+              <label>Tags</label>
+              <Dropdown
+                options={this.state.options}
+                placeholder='Choose Tags'
+                search
+                selection
+                fluid
+                multiple
+                allowAdditions
+                value={currentValues}
+                onAddItem={this.handleAddition}
+                onChange={this.handleChange}
+              />
+            </FormField>
+            <Button type='submit'>Submit</Button>
+          </FormGroup>
+        </Form>
+      </div>
     )
   }
 }
